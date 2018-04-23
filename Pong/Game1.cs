260c9972e8f8e75c8fe0c65b2f3da60cde673dae
Linus Juhlin <juhlinus@gmail.com>
2018-04-23 13:12:53 +0800

    Add project files.

---
+++ Pong/Game1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using Nez.Sprites;
+
+namespace Pong
+{
+    public class Game1 : Core
+    {
+        public Game1() {}
+        
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            var myScene = Scene.createWithDefaultRenderer(Color.CornflowerBlue);
+
+            var text = new Text(Graphics.instance.bitmapFont, "Hello Pong!", new Vector2(20, 200), Color.Black);
+
+            var textEntity = myScene.createEntity("text");
+
+            textEntity.addComponent(text);
+
+            textEntity.scale = new Vector2(25);
+
+            scene = myScene;
+        }
+    }
+}
